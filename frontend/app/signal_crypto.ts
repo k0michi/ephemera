ef18be1db9843a627dea7342b04b74fb5ad26dd7
k0michi <k0michi@koyomi.co>
2025-12-23 21:32:59 +0900

    fix: correct typings

--- frontend/app/signal_crypto.ts
+++ frontend/app/signal_crypto.ts
@@ -3,22 +3,20 @@ import Crypto from "./crypto.js";
 import Hex from "./hex.js";
 
 export default class SignalCrypto {
-  static async digest
-    (signal: SignalPayload): Promise<Uint8Array> {
+  static async digest<S extends SignalPayload>(signal: S): Promise<Uint8Array> {
     const payloadString = JSON.stringify(signal);
     const payloadUint8 = new TextEncoder().encode(payloadString);
     return await Crypto.digest(payloadUint8);
   }
 
-  static async sign<Header, Body, Footer>
-    (signal: SignalPayload, privateKey: Uint8Array): Promise<Signal> {
+  static async sign<S extends SignalPayload>(signal: S, privateKey: Uint8Array): Promise<[S, string]> {
     const payloadHash = await SignalCrypto.digest(signal);
     const signatureUint8 = Crypto.sign(payloadHash, privateKey);
     const signatureHex = Hex.fromUint8Array(signatureUint8);
     return [signal, signatureHex];
   }
 
-  static async verify(signal: Signal): Promise<boolean> {
+  static async verify<S extends Signal>(signal: S): Promise<boolean> {
     const [payload, signatureHex] = signal;
     const payloadHash = await SignalCrypto.digest(payload);
     const signatureUint8 = Hex.toUint8Array(signatureHex);
