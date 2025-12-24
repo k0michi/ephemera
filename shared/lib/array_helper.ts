b7b77cde581920497b60a7d5f817cb45fc0393c7
k0michi <k0michi@koyomi.co>
2025-12-24 13:20:38 +0900

    fix: correct behavior of strictGet when array has undefined

--- shared/lib/array_helper.ts
+++ shared/lib/array_helper.ts
@@ -1,12 +1,10 @@
 export default class ArrayHelper {
   static strictGet<T>(array: ArrayLike<T>, index: number): T {
-    const result = array[index];
-
-    if (result === undefined) {
+    if (index < 0 || index >= array.length) {
       throw new Error(`Index ${index} is out of bounds for array of length ${array.length}.`);
     }
 
-    return result;
+    return array[index] as T;
   }
 
   static strictSet<T>(array: ArrayLike<T> & { [index: number]: T }, index: number, value: T): void {
