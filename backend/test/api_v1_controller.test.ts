bcff2ae568009440c34cb887f54cdedf0c3ffbb9
k0michi <k0michi@koyomi.co>
2025-12-24 17:50:36 +0900

    test: add test case for valid request

--- backend/test/api_v1_controller.test.ts
+++ backend/test/api_v1_controller.test.ts
@@ -1,8 +1,11 @@
 import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
 import { createRequest, createResponse } from 'node-mocks-http';
 import ApiV1Controller from '../app/api_v1_controller.js';
-import type { PostRequest } from '@ephemera/shared/api/api.js';
+import type { CreatePostSignalPayload, PostRequest } from '@ephemera/shared/api/api.js';
 import Config from '../app/config.js';
+import Crypto from '@ephemera/shared/lib/crypto.js';
+import SignalCrypto from '@ephemera/shared/lib/signal_crypto.js';
+import Base37 from '@ephemera/shared/lib/base37.js';
 
 function testConfig() {
   return new Config({ host: 'example.com', port: 3000 });
@@ -44,6 +47,32 @@ describe('ApiV1Controller', () => {
       expect(res.statusCode).toBe(400);
       const data = res._getJSONData();
       expect(data).toHaveProperty('error', 'Invalid signature');
+    });
+
+    it('should respond with 200 for valid post', async () => {
+      const keyPair = Crypto.generateKeyPair();
+      const now = Date.now();
+      const signedSignal = await SignalCrypto.sign(
+        [0, ['example.com', Base37.fromUint8Array(keyPair.publicKey), now - 1000, 'create_post'], 'body text', []] satisfies CreatePostSignalPayload,
+        keyPair.privateKey
+      );
+
+      const req = createRequest({
+        method: 'POST',
+        url: '/api/v1/post',
+        body: {
+          post: signedSignal,
+        } satisfies PostRequest,
+      });
+      const res = createResponse();
+
+      const config = testConfig();
+
+      const controller = new ApiV1Controller(config);
+      await controller.handlePost(req, res);
+      expect(res.statusCode).toBe(200);
+      const data = res._getJSONData();
+      expect(data).toEqual({});
     });
   });
 });
