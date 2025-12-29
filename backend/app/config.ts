6b4d5ab948860c28549d6a8cc506b4921fd41800
k0michi <k0michi@koyomi.co>
2025-12-29 11:28:33 +0900

    feat: add more robust environment parsing

--- backend/app/config.ts
+++ backend/app/config.ts
@@ -1,5 +1,3 @@
-import NullableHelper from "@ephemera/shared/lib/nullable_helper.js";
-
 export default class Config {
   /**
    * Public host name. The port may be different from the listening port.
@@ -29,15 +27,67 @@ export default class Config {
     this.allowedTimeSkewMillis = allowedTimeSkewMillis;
   }
 
+  private static unwrapStringMandatory(env: NodeJS.ProcessEnv, key: string): string {
+    const value = env[key];
+
+    if (value === undefined) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+
+    return value;
+  }
+
+  private static unwrapNumberMandatory(env: NodeJS.ProcessEnv, key: string): number {
+    const value = env[key];
+
+    if (value === undefined) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+
+    const num = Number(value);
+
+    if (isNaN(num)) {
+      throw new Error(`Environment variable ${key} is not a valid number`);
+    }
+
+    return num;
+  }
+
+  private static unwrapStringOptional(env: NodeJS.ProcessEnv, key: string, defaultValue: string): string {
+    const value = env[key];
+
+    if (value === undefined) {
+      return defaultValue;
+    }
+
+    return value;
+  }
+
+  private static unwrapNumberOptional(env: NodeJS.ProcessEnv, key: string, defaultValue: number): number {
+    const value = env[key];
+
+    if (value === undefined) {
+      return defaultValue;
+    }
+
+    const num = Number(value);
+
+    if (isNaN(num)) {
+      return defaultValue;
+    }
+
+    return num;
+  }
+
   static fromEnv(): Config {
-    const host = NullableHelper.unwrap(process.env.EPHEMERA_HOST);
-    const port = Number(NullableHelper.unwrap(process.env.EPHEMERA_PORT));
-    const dbHost = NullableHelper.unwrap(process.env.EPHEMERA_DB_HOST);
-    const dbPort = Number(NullableHelper.unwrap(process.env.EPHEMERA_DB_PORT));
-    const dbUser = NullableHelper.unwrap(process.env.EPHEMERA_DB_USER);
-    const dbPassword = NullableHelper.unwrap(process.env.EPHEMERA_DB_PASSWORD);
-    const dbName = NullableHelper.unwrap(process.env.EPHEMERA_DB_NAME);
-    const allowedTimeSkewMillis = NullableHelper.map(process.env.EPHEMERA_ALLOWED_TIME_SKEW_MILLIS, (val) => Number(val)) ?? Config._kDefaultAllowedTimeSkewMillis;
+    const host = Config.unwrapStringMandatory(process.env, 'EPHEMERA_HOST');
+    const port = Config.unwrapNumberMandatory(process.env, 'EPHEMERA_PORT');
+    const dbHost = Config.unwrapStringMandatory(process.env, 'EPHEMERA_DB_HOST');
+    const dbPort = Config.unwrapNumberMandatory(process.env, 'EPHEMERA_DB_PORT');
+    const dbUser = Config.unwrapStringMandatory(process.env, 'EPHEMERA_DB_USER');
+    const dbPassword = Config.unwrapStringMandatory(process.env, 'EPHEMERA_DB_PASSWORD');
+    const dbName = Config.unwrapStringMandatory(process.env, 'EPHEMERA_DB_NAME');
+    const allowedTimeSkewMillis = Config.unwrapNumberOptional(process.env, 'EPHEMERA_ALLOWED_TIME_SKEW_MILLIS', Config._kDefaultAllowedTimeSkewMillis);
     return new Config({ host, port, dbHost, dbPort, dbUser, dbPassword, dbName, allowedTimeSkewMillis });
   }
 }
