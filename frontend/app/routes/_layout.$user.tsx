bec0d200a89ee67772c85c91a2ce2232df39b43b
k0michi <k0michi@koyomi.co>
2026-01-03 19:06:20 +0900

    style: fix inconsistent font family

--- frontend/app/routes/_layout.$user.tsx
+++ frontend/app/routes/_layout.$user.tsx
@@ -31,7 +31,6 @@ export default function User() {
               overflow: 'hidden',
               textOverflow: 'ellipsis',
               whiteSpace: 'nowrap',
-              fontFamily: 'monospace',
             }}
           >
             @{userKey}
