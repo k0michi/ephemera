89e9663bb2cc83974b2ffd7fe3e7cfc6398352ae
k0michi <k0michi@koyomi.co>
2026-02-03 21:42:58 +0900

    feat: cache favicon

--- frontend/app/routes/favicon[.]ico.ts
+++ frontend/app/routes/favicon[.]ico.ts
@@ -1,12 +1,12 @@
 import { render } from "components/server_identicon";
-import NullableHelper from "@ephemera/shared/lib/nullable_helper";
 import sharp from "sharp";
 import pngToIco from "png-to-ico";
 import Crypto from "@ephemera/shared/lib/crypto";
+import { Cache } from "@ephemera/shared/lib/cache";
 
 const kSizes = [16, 32, 48, 64, 128, 256];
 
-async function renderToIco(svgString: string): Promise<Buffer> {
+async function renderToIco(svgString: string) {
   const svgBuf = Buffer.from(svgString, 'utf-8');
 
   const pngs = await Promise.all(
@@ -21,17 +21,30 @@ async function renderToIco(svgString: string): Promise<Buffer> {
   return pngToIco(pngs);
 }
 
-export async function loader() {
-  const host = NullableHelper.unwrap(process.env.EPHEMERA_HOST);
-  const digest = await Crypto.digest(new TextEncoder().encode(host));
-  const svgString = render(digest, { numSegments: 7, gapWidth: 16 });
-  const icoBuffer = await renderToIco(svgString);
+const icoCache = new Cache<Uint8Array | null>();
 
-  return new Response(new Uint8Array(icoBuffer), {
+export async function loader() {
+  const ico = await icoCache.getOrSet(async () => {
+    const host = process.env.EPHEMERA_HOST;
+
+    if (host === undefined) {
+      return null;
+    }
+
+    const digest = await Crypto.digest(new TextEncoder().encode(host));
+    const svgString = render(digest, { numSegments: 7, gapWidth: 16 });
+    return await renderToIco(svgString);
+  });
+
+  if (ico === null) {
+    return new Response("Not Found", { status: 404 });
+  }
+
+  return new Response(new Uint8Array(ico), {
     status: 200,
     headers: {
       "Content-Type": "image/x-icon",
-      "Content-Length": icoBuffer.length.toString(),
+      "Content-Length": ico.length.toString(),
       "Cache-Control": "public, max-age=86400"
     }
   });
