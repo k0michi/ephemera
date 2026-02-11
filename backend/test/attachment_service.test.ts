a63f1f99c15c63a1acdd64369b0a07ced5652948
k0michi <k0michi@koyomi.co>
2026-02-11 15:15:59 +0900

    test: add attachment test

---
+++ backend/test/attachment_service.test.ts
@@ -0,0 +1,49 @@
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { AttachmentService } from "../app/attachment_service.js";
+import { drizzle, type MySql2Database } from "drizzle-orm/mysql2";
+import type { Pool } from "mysql2";
+import type { StartedMariaDbContainer } from "@testcontainers/mariadb";
+import TestHelper from "./test_helper.js";
+import { migrate } from "drizzle-orm/mysql2/migrator";
+import FSHelper from "../app/fs_helper.js";
+
+describe('AttachmentService', () => {
+  let container: StartedMariaDbContainer;
+  let database: MySql2Database;
+  let pool: Pool;
+  let attachmentService: AttachmentService;
+
+  beforeEach(async () => {
+    container = await TestHelper.startDbContainer();
+
+    const db = drizzle(container.getConnectionUri());
+    pool = db.$client;
+    database = db;
+    await migrate(db, { migrationsFolder: './drizzle' });
+
+    const config = TestHelper.getConfig(container);
+    attachmentService = new AttachmentService(config, database);
+  }, 60_000);
+
+  afterEach(async () => {
+    await TestHelper.endPool(pool);
+    await container.stop();
+  });
+
+  it('should create a new attachment from a file', async () => {
+    await database.transaction(async (tx) => {
+      const testImage = await TestHelper.newDummyImage(
+        800,
+        600,
+        'png',
+        true
+      );
+
+      const attachmentId = await attachmentService.copyFrom(testImage, tx);
+
+      const filePath = attachmentService.getFilePath(attachmentId);
+
+      await TestHelper.assertFileEquals(testImage, filePath);
+    });
+  });
+});
--- backend/test/test_helper.ts
+++ backend/test/test_helper.ts
@@ -1,6 +1,11 @@
 import { MariaDbContainer, type StartedMariaDbContainer } from "@testcontainers/mariadb";
 import Config from "../app/config.js";
 import type { Pool } from "mysql2";
+import fsPromises from 'fs/promises';
+import sharp from "sharp";
+import { expect } from "vitest";
+import path from "path";
+import os from "os";
 
 export default class TestHelper {
   static startDbContainer() {
@@ -38,5 +43,54 @@ export default class TestHelper {
         }
       });
     });
+  }
+
+  static async newTempFile(): Promise<string> {
+    const dir = await fsPromises.mkdtemp(path.join(os.tmpdir(), "ephemera-test-"));
+    const name = `file-${crypto.randomUUID()}`;
+    const filePath = path.join(dir, name);
+    return filePath;
+  }
+
+  static async newDummyImage(width: number, height: number, format: 'png' | 'jpeg' | 'webp' | 'gif', alpha: boolean): Promise<string> {
+    const size = width * height * (alpha ? 4 : 3);
+    const randomBuffer = Buffer.alloc(size);
+
+    for (let i = 0; i < size; i++) {
+      randomBuffer[i] = Math.floor(Math.random() * 256);
+    }
+
+    let image = sharp(randomBuffer, {
+      raw: {
+        width: width,
+        height: height,
+        channels: alpha ? 4 : 3,
+      }
+    });
+
+    switch (format) {
+      case 'png':
+        image = image.png();
+        break;
+      case 'jpeg':
+        image = image.jpeg();
+        break;
+      case 'webp':
+        image = image.webp();
+        break;
+      case 'gif':
+        image = image.gif();
+        break;
+    }
+
+    const filePath = await this.newTempFile();
+    await image.toFile(filePath);
+    return filePath;
+  }
+
+  static async assertFileEquals(filePath1: string, filePath2: string): Promise<void> {
+    const buffer1 = await fsPromises.readFile(filePath1);
+    const buffer2 = await fsPromises.readFile(filePath2);
+    expect(buffer1).toEqual(buffer2);
   }
 }
