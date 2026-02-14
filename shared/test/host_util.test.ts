f17fb1395bb4dc12da8fd765d33c8c5560cdb1bd
k0michi <k0michi@koyomi.co>
2026-02-14 20:29:39 +0900

    test: fix test names

--- shared/test/host_util.test.ts
+++ shared/test/host_util.test.ts
@@ -3,7 +3,7 @@ import HostUtil from "../lib/host_util.js";
 
 describe('HostUtil', () => {
   describe('isValid', () => {
-    it('should validate strict host strings', () => {
+    it('should validate valid hosts', () => {
       expect(HostUtil.isValid('example.com')).toBe(true);
       expect(HostUtil.isValid('example.com:8080')).toBe(true);
       expect(HostUtil.isValid('localhost:3000')).toBe(true);
@@ -12,7 +12,7 @@ describe('HostUtil', () => {
       expect(HostUtil.isValid('example.com:443')).toBe(true);
     });
 
-    it('should invalidate invalid strict host strings', () => {
+    it('should invalidate invalid hosts', () => {
       expect(HostUtil.isValid(':8080')).toBe(false);
       expect(HostUtil.isValid('http://example.com:8080')).toBe(false);
       expect(HostUtil.isValid('https://example.com:8080')).toBe(false);
