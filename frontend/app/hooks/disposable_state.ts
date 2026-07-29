b0add1ac705fdb62431fe0273025742c58dc3677
k0michi <k0michi@koyomi.co>
2026-07-29 10:05:46 +0900

    fix: defer resource disposition when rendered

--- frontend/app/hooks/disposable_state.ts
+++ frontend/app/hooks/disposable_state.ts
@@ -6,29 +6,34 @@ export function useDisposableState<T extends Disposable>(): [
   Dispatch<SetStateAction<T | null>>
 ] {
   const [resource, setResourceState] = useState<T | null>(null);
-  const resourceRef = useRef<T | null>(null);
+  const requestedRef = useRef<T | null>(null);
+  const committedRef = useRef<T | null>(null);
 
   const setResource = useCallback<Dispatch<SetStateAction<T | null>>>(action => {
-    const prev = resourceRef.current;
+    const prev = requestedRef.current;
     const next = typeof action === "function"
       ? (action as (prev: T | null) => T | null)(prev)
       : action;
 
-    if (prev && prev !== next) {
+    if (prev && prev !== next && prev !== committedRef.current) {
+      // setResource has been called multiple times in a single tick
       prev[SymbolHelper.dispose]();
     }
 
-    resourceRef.current = next;
+    requestedRef.current = next;
     setResourceState(next);
   }, []);
 
   useEffect(() => {
+    // resource is the value that is going to be rendered, and it should be clean up in the destruction callback
+    committedRef.current = resource;
+
     return () => {
-      if (resourceRef.current) {
-        resourceRef.current[SymbolHelper.dispose]();
+      if (resource) {
+        resource[SymbolHelper.dispose]();
       }
     };
-  }, []);
+  }, [resource]);
 
   return [resource, setResource];
 }
